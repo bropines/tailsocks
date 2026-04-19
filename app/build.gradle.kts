ba55104a78af6e0d511d9ad6f323e760901e0b61
bropines <bropines@gmail.com>
2026-04-19 23:53:54 +0300

    build: fix JVM toolchain and Gradle tasks for dev build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,6 +83,15 @@ android {
         }
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlin {
+        jvmToolchain(17)
+    }
+
     splits {
         abi {
             isEnable = true
