858209045d668590e62294fc8abf38717eeec74a
bropines <bropines@gmail.com>
2026-06-02 14:57:21 +0300

    i18n: add appcompat dependency and AppLocalesMetadataHolderService to manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -115,6 +115,7 @@ dependencies {
     implementation(project(":appctr"))
     implementation(libs.gson)
     implementation("androidx.biometric:biometric:1.1.0")
+    implementation(libs.androidx.appcompat)
     
     // ВАЖНО: Библиотека для XML-тем (исправляет "resource style/Theme.Material3... not found")
     implementation(libs.material) 
